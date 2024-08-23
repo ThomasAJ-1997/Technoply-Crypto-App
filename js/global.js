f5733a0238015d1774c71f9a65f870123db037cf
Thomas Jones <Thomasajones@hotmail.co.uk>
2024-08-23 12:00:33 +0100

    global section commit

--- js/global.js
+++ js/global.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const coinsCount = document.getElementById("coinsID");
+const exchangeCount = document.getElementById("exchangeID");
+const marketCount = document.getElementById("marketID");
+const marketChangeCount = document.getElementById("marketChangeID");
+const volume = document.getElementById("volumeID");
+const dominance = document.getElementById("dominanceID");
 //////////////////////////////////////////////////////////////////////////////////////
 // LOCAL STORAGE AND COINGECKO API
 
@@ -46,7 +51,50 @@ function fetchGlobal() {
         displayGlobalData(data);
         setLocalStorageData(localStorageKey, globalData);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        coinsCount.textContent = "N/A";
+        exchangeCount.textContent = "N/A";
+        marketCount.textContent = "N/A";
+        marketChangeCount.textContent = "N/A";
+        volume.textContent = "N/A";
+        dominance.textContent = "BTC N/A% - ETH N/A%";
+        console.error(error);
+      });
   }
 }
-//////////////////////////////////////////////////////////////////////////////////////
+
+function displayGlobalData(globalData) {
+  coinsCount.textContent = globalData.active_cryptocurrencies || "N/A";
+  exchangeCount.textContent = globalData.markets || "N/A";
+
+  marketCount.textContent = globalData.total_market_cap?.gbp
+    ? `££${(globalData.total_market_cap.gbp / 1e12).toFixed(3)}T`
+    : "N/A";
+
+  const marketCapChange = globalData.market_cap_change_percentage_24h_usd;
+  // No GDP option for public API: InnerHTML and icon change depending on the Market Cap %
+  if (marketCapChange !== undefined) {
+    const changeText = `${marketCapChange.toFixed(1)}%`;
+    marketChangeCount.innerHTML = `${changeText} <i class="${
+      marketCapChange < 0 ? "red" : "green"
+    } ri-arrow-${marketCapChange < 0 ? "down" : "up"}-s-fill"></i>`;
+    marketChangeCount.style.color = marketCapChange < 0 ? "red" : "green";
+  } else {
+    marketChangeCount.textContent = "N/A";
+  }
+
+  volume.textContent = globalData.total_volume?.gbp
+    ? `£${(globalData.total_volume.gbp / 1e9).toFixed(3)}B`
+    : "N/A";
+
+  const btcDominance = globalData.market_cap_percentage?.btc
+    ? `${globalData.market_cap_percentage.btc.toFixed(1)}%`
+    : "N/A";
+
+  const ethDominance = globalData.market_cap_percentage?.eth
+    ? `${globalData.market_cap_percentage.eth.toFixed(1)}%`
+    : "N/A";
+
+  dominance.textContent = `BTC ${btcDominance} - ETH ${ethDominance}`;
+}
+/////////////////////////////////////////////////////////////////////////////////////
